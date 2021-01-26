f60b774e636225a1f1dd0ec8422ee27f2dd6de94
Ozasnidev <ozasnidev@gmail.com>
2021-01-26 13:55:02 +0100

    10 - Mobile menu animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,20 @@
 const btnHamburger = document.querySelector('#btnMobileMenu');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElem = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', ()=> {
     if(header.classList.contains('open')){
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out'); 
+        fadeElem.forEach(element =>{
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        }); 
     }else{
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElem.forEach(element =>{
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
     } 
 })
