837bfd86a06c6340aa05121895eae2c6fd42c6e1
Ozasnidev <ozasnidev@gmail.com>
2021-01-26 11:41:15 +0100

    06 - Mobile menu animation and open/close click toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
-console.log("Success!");
+const btnHamburger = document.querySelector('#btnMobileMenu');
+
+btnHamburger.addEventListener('click', ()=> {
+   btnHamburger.classList.contains('open') 
+    ? btnHamburger.classList.remove('open') 
+    : btnHamburger.classList.add('open');
+})
