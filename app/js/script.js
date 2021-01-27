a2bf5cc16ae6482f35c87ee0ef7433f10902bee9
Ozasnidev <ozasnidev@gmail.com>
2021-01-27 17:04:47 +0100

    11 - Block body scroll on mobile menu opened

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,20 @@
-const btnHamburger = document.querySelector('#btnMobileMenu');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
+
+const btnHamburger = document.querySelector('#btnMobileMenu');
 const overlay = document.querySelector('.overlay');
 const fadeElem = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', ()=> {
     if(header.classList.contains('open')){
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         fadeElem.forEach(element =>{
             element.classList.add('fade-out');
             element.classList.remove('fade-in');
         }); 
     }else{
+        body.classList.add('noscroll');
         header.classList.add('open');
         fadeElem.forEach(element =>{
             element.classList.remove('fade-out');
